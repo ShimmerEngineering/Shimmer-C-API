27603e7e6f32030eb197ddbad8210453d2689d94
Jong Chern <jongchern@gmail.com>
2017-04-12 11:17:19 +0800

    Minor updates to show different constructors available, one which automatically configures the shimmer device upon connection and one which doesn't

--- ShimmerConsoleAppExample/ShimmerConsoleAppExample/Program.cs
+++ ShimmerConsoleAppExample/ShimmerConsoleAppExample/Program.cs
@@ -25,12 +25,19 @@ namespace ShimmerConsoleAppExample
 
         public void start()
         {
-            int enabledSensors = ((int)Shimmer.SensorBitmapShimmer3.SENSOR_A_ACCEL); // this is to enable Accel
-            shimmer = new ShimmerSDBT("ShimmerID1", "COM15");
+            //There are two main uses of the constructors, first one just connects to the device without setting and specific configurations
+            //shimmer = new ShimmerSDBT("ShimmerID1", "COM15");
+            int enabledSensors = ((int)Shimmer.SensorBitmapShimmer3.SENSOR_A_ACCEL | (int)Shimmer.SensorBitmapShimmer3.SENSOR_GSR); // this is to enable Accel
+            double samplingRate = 51.2;
+            byte[] defaultECGReg1 = new byte[10] { 0x00, 0xA0, 0x10, 0x40, 0x40, 0x2D, 0x00, 0x00, 0x02, 0x03 };
+            byte[] defaultECGReg2 = new byte[10] { 0x00, 0xA0, 0x10, 0x40, 0x47, 0x00, 0x00, 0x00, 0x02, 0x01 };
+            //The constructor below allows the user to specify the shimmer configurations which is set upon connection to the device
+            shimmer = new ShimmerSDBT("ShimmerID1", "COM12", 1, 0, 4, enabledSensors, false, false, false, 0, 0, defaultECGReg1, defaultECGReg2, false);
             shimmer.UICallback += this.HandleEvent;
             shimmer.Connect();
             if (shimmer.GetState() == Shimmer.SHIMMER_STATE_CONNECTED)
             {
+                shimmer.WriteSamplingRate(samplingRate);
                 shimmer.WriteSensors(enabledSensors);
                 shimmer.StartStreaming();
             }
