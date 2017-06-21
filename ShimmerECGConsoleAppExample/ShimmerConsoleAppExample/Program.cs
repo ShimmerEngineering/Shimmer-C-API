c2cb9976716f6e5cdc47fe4ab718cde0742923d5
Jong Chern <jongchern@gmail.com>
2017-06-21 14:36:04 +0800

    add a ecg example

---
+++ ShimmerECGConsoleAppExample/ShimmerConsoleAppExample/Program.cs
@@ -0,0 +1,85 @@
+﻿using ShimmerAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShimmerConsoleAppExample
+{
+    class Program
+    {
+        ShimmerSDBT shimmer;
+
+        static void Main(string[] args)
+        {
+            /* Example of using 32 feet to scan for devices
+            var client = new BluetoothClient();
+            BluetoothDeviceInfo[] availableDevices = client.DiscoverDevices(); // I've found this to be SLOW!
+            */
+
+            System.Console.WriteLine("Hello");
+            Program p = new Program();
+            p.start();
+        }
+
+        public void start()
+        {
+            //There are two main uses of the constructors, first one just connects to the device without setting and specific configurations
+            //shimmer = new ShimmerSDBT("ShimmerID1", "COM15");
+            int enabledSensors = ((int)Shimmer.SensorBitmapShimmer3.SENSOR_EXG1_24BIT | (int)Shimmer.SensorBitmapShimmer3.SENSOR_EXG2_24BIT); // this is to enable the two EXG Chips on the Shimmer3
+            double samplingRate = 51.2;
+            //byte[] defaultECGReg1 = new byte[10] { 0x00, 0xA0, 0x10, 0x40, 0x40, 0x2D, 0x00, 0x00, 0x02, 0x03 }; //see ShimmerBluetooth.SHIMMER3_DEFAULT_ECG_REG1
+            //byte[] defaultECGReg2 = new byte[10] { 0x00, 0xA0, 0x10, 0x40, 0x47, 0x00, 0x00, 0x00, 0x02, 0x01 }; //see ShimmerBluetooth.SHIMMER3_DEFAULT_ECG_REG2
+            byte[] defaultECGReg1 = ShimmerBluetooth.SHIMMER3_DEFAULT_ECG_REG1; //also see ShimmerBluetooth.SHIMMER3_DEFAULT_TEST_REG1 && ShimmerBluetooth.SHIMMER3_DEFAULT_EMG_REG1
+            byte[] defaultECGReg2 = ShimmerBluetooth.SHIMMER3_DEFAULT_ECG_REG2; //also see ShimmerBluetooth.SHIMMER3_DEFAULT_TEST_REG2 && ShimmerBluetooth.SHIMMER3_DEFAULT_EMG_REG2
+            //The constructor below allows the user to specify the shimmer configurations which is set upon connection to the device
+            shimmer = new ShimmerSDBT("ShimmerID1", "COM35", 1, 0, 4, enabledSensors, false, false, false, 0, 0, defaultECGReg1, defaultECGReg2, false);
+            shimmer.UICallback += this.HandleEvent;
+            shimmer.Connect();
+            if (shimmer.GetState() == Shimmer.SHIMMER_STATE_CONNECTED)
+            {
+                shimmer.WriteSamplingRate(samplingRate);
+                shimmer.WriteSensors(enabledSensors);
+                shimmer.StartStreaming();
+            }
+        }
+        public void HandleEvent(object sender, EventArgs args)
+        {
+            CustomEventArgs eventArgs = (CustomEventArgs)args;
+            int indicator = eventArgs.getIndicator();
+
+            switch (indicator)
+            {
+                case (int)Shimmer.ShimmerIdentifier.MSG_IDENTIFIER_STATE_CHANGE:
+                    System.Diagnostics.Debug.Write(((Shimmer)sender).GetDeviceName() + " State = " + ((Shimmer)sender).GetStateString() + System.Environment.NewLine);
+                    int state = (int)eventArgs.getObject();
+                    if (state == (int)Shimmer.SHIMMER_STATE_CONNECTED)
+                    {
+
+                    }
+                    else if (state == (int)Shimmer.SHIMMER_STATE_CONNECTING)
+                    {
+
+                    }
+                    else if (state == (int)Shimmer.SHIMMER_STATE_NONE)
+                    {
+                        System.Console.WriteLine("Disconnected");
+
+                    }
+                    else if (state == (int)Shimmer.SHIMMER_STATE_STREAMING)
+                    {
+
+                    }
+                    break;
+                case (int)Shimmer.ShimmerIdentifier.MSG_IDENTIFIER_NOTIFICATION_MESSAGE:
+                    break;
+                case (int)Shimmer.ShimmerIdentifier.MSG_IDENTIFIER_DATA_PACKET:
+                    ObjectCluster objectCluster = (ObjectCluster)eventArgs.getObject();
+                    SensorData data = objectCluster.GetData(Shimmer3Configuration.SignalNames.ECG_LA_RA, "CAL");
+                    System.Console.Write(data.Data+",");
+                    break;
+            }
+        }
+    }
+}
