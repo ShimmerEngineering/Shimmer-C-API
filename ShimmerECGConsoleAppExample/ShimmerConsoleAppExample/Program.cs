3204866f034c534e442f56105ccb48421988ac59
Jong Chern <jongchern@gmail.com>
2017-07-19 17:54:45 +0800

    minor updates to use the new ShimmerLogAndStream Classes

--- ShimmerECGConsoleAppExample/ShimmerConsoleAppExample/Program.cs
+++ ShimmerECGConsoleAppExample/ShimmerConsoleAppExample/Program.cs
@@ -9,7 +9,7 @@ namespace ShimmerConsoleAppExample
 {
     class Program
     {
-        ShimmerSDBT shimmer;
+        ShimmerLogAndStreamSystemSerialPort shimmer;
 
         static void Main(string[] args)
         {
@@ -27,17 +27,17 @@ namespace ShimmerConsoleAppExample
         {
             //There are two main uses of the constructors, first one just connects to the device without setting and specific configurations
             //shimmer = new ShimmerSDBT("ShimmerID1", "COM15");
-            int enabledSensors = ((int)Shimmer.SensorBitmapShimmer3.SENSOR_EXG1_24BIT | (int)Shimmer.SensorBitmapShimmer3.SENSOR_EXG2_24BIT); // this is to enable the two EXG Chips on the Shimmer3
+            int enabledSensors = ((int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_EXG1_24BIT | (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_EXG2_24BIT); // this is to enable the two EXG Chips on the Shimmer3
             double samplingRate = 51.2;
             //byte[] defaultECGReg1 = new byte[10] { 0x00, 0xA0, 0x10, 0x40, 0x40, 0x2D, 0x00, 0x00, 0x02, 0x03 }; //see ShimmerBluetooth.SHIMMER3_DEFAULT_ECG_REG1
             //byte[] defaultECGReg2 = new byte[10] { 0x00, 0xA0, 0x10, 0x40, 0x47, 0x00, 0x00, 0x00, 0x02, 0x01 }; //see ShimmerBluetooth.SHIMMER3_DEFAULT_ECG_REG2
             byte[] defaultECGReg1 = ShimmerBluetooth.SHIMMER3_DEFAULT_ECG_REG1; //also see ShimmerBluetooth.SHIMMER3_DEFAULT_TEST_REG1 && ShimmerBluetooth.SHIMMER3_DEFAULT_EMG_REG1
             byte[] defaultECGReg2 = ShimmerBluetooth.SHIMMER3_DEFAULT_ECG_REG2; //also see ShimmerBluetooth.SHIMMER3_DEFAULT_TEST_REG2 && ShimmerBluetooth.SHIMMER3_DEFAULT_EMG_REG2
             //The constructor below allows the user to specify the shimmer configurations which is set upon connection to the device
-            shimmer = new ShimmerSDBT("ShimmerID1", "COM35", 1, 0, 4, enabledSensors, false, false, false, 0, 0, defaultECGReg1, defaultECGReg2, false);
+            shimmer = new ShimmerLogAndStreamSystemSerialPort("ShimmerID1", "COM35", 1, 0, 4, enabledSensors, false, false, false, 0, 0, defaultECGReg1, defaultECGReg2, false);
             shimmer.UICallback += this.HandleEvent;
             shimmer.Connect();
-            if (shimmer.GetState() == Shimmer.SHIMMER_STATE_CONNECTED)
+            if (shimmer.GetState() == ShimmerBluetooth.SHIMMER_STATE_CONNECTED)
             {
                 shimmer.WriteSamplingRate(samplingRate);
                 shimmer.WriteSensors(enabledSensors);
@@ -51,30 +51,29 @@ namespace ShimmerConsoleAppExample
 
             switch (indicator)
             {
-                case (int)Shimmer.ShimmerIdentifier.MSG_IDENTIFIER_STATE_CHANGE:
-                    System.Diagnostics.Debug.Write(((Shimmer)sender).GetDeviceName() + " State = " + ((Shimmer)sender).GetStateString() + System.Environment.NewLine);
+                case (int)ShimmerBluetooth.ShimmerIdentifier.MSG_IDENTIFIER_STATE_CHANGE:
+                    System.Diagnostics.Debug.Write(((ShimmerBluetooth)sender).GetDeviceName() + " State = " + ((ShimmerBluetooth)sender).GetStateString() + System.Environment.NewLine);
                     int state = (int)eventArgs.getObject();
-                    if (state == (int)Shimmer.SHIMMER_STATE_CONNECTED)
+                    if (state == (int)ShimmerBluetooth.SHIMMER_STATE_CONNECTED)
                     {
-
+                        System.Console.WriteLine("Connected");
                     }
-                    else if (state == (int)Shimmer.SHIMMER_STATE_CONNECTING)
+                    else if (state == (int)ShimmerBluetooth.SHIMMER_STATE_CONNECTING)
                     {
-
+                        System.Console.WriteLine("Connecting");
                     }
-                    else if (state == (int)Shimmer.SHIMMER_STATE_NONE)
+                    else if (state == (int)ShimmerBluetooth.SHIMMER_STATE_NONE)
                     {
                         System.Console.WriteLine("Disconnected");
-
                     }
-                    else if (state == (int)Shimmer.SHIMMER_STATE_STREAMING)
+                    else if (state == (int)ShimmerBluetooth.SHIMMER_STATE_STREAMING)
                     {
-
+                        System.Console.WriteLine("Streaming");
                     }
                     break;
-                case (int)Shimmer.ShimmerIdentifier.MSG_IDENTIFIER_NOTIFICATION_MESSAGE:
+                case (int)ShimmerBluetooth.ShimmerIdentifier.MSG_IDENTIFIER_NOTIFICATION_MESSAGE:
                     break;
-                case (int)Shimmer.ShimmerIdentifier.MSG_IDENTIFIER_DATA_PACKET:
+                case (int)ShimmerBluetooth.ShimmerIdentifier.MSG_IDENTIFIER_DATA_PACKET:
                     ObjectCluster objectCluster = (ObjectCluster)eventArgs.getObject();
                     SensorData data = objectCluster.GetData(Shimmer3Configuration.SignalNames.ECG_LA_RA, "CAL");
                     System.Console.Write(data.Data+",");
--- ShimmerPPGHRGSRConsoleAppExample/ShimmerConsoleAppExample/Program.cs
+++ ShimmerPPGHRGSRConsoleAppExample/ShimmerConsoleAppExample/Program.cs
@@ -17,7 +17,7 @@ namespace ShimmerConsoleAppExample
         int TrainingPeriodPPG = 10; //10 second buffer
         double LPF_CORNER_FREQ_HZ = 5;
         double HPF_CORNER_FREQ_HZ = 0.5;
-        ShimmerSDBT Shimmer;
+        ShimmerLogAndStreamSystemSerialPort Shimmer;
         double SamplingRate = 128;
         int Count = 0;
         bool FirstTime = true;
@@ -49,7 +49,7 @@ namespace ShimmerConsoleAppExample
             //int enabledSensors = ((int)Shimmer.SensorBitmapShimmer3.SENSOR_A_ACCEL | (int)Shimmer.SensorBitmapShimmer3.SENSOR_EXG1_24BIT | (int)Shimmer.SensorBitmapShimmer3.SENSOR_EXG2_24BIT); 
 
             //shimmer = new Shimmer("ShimmerID1", "COM17");
-            Shimmer = new ShimmerSDBT("ShimmerID1", "COM12", SamplingRate, 0, ShimmerBluetooth.GSR_RANGE_AUTO, enabledSensors, false, false, false, 1, 0, Shimmer3Configuration.EXG_EMG_CONFIGURATION_CHIP1, Shimmer3Configuration.EXG_EMG_CONFIGURATION_CHIP2, true);
+            Shimmer = new ShimmerLogAndStreamSystemSerialPort("ShimmerID1", "COM12", SamplingRate, 0, ShimmerBluetooth.GSR_RANGE_AUTO, enabledSensors, false, false, false, 1, 0, Shimmer3Configuration.EXG_EMG_CONFIGURATION_CHIP1, Shimmer3Configuration.EXG_EMG_CONFIGURATION_CHIP2, true);
 
             Shimmer.UICallback += this.HandleEvent;
             Shimmer.Connect();
@@ -63,7 +63,7 @@ namespace ShimmerConsoleAppExample
             switch (indicator)
             {
                 case (int)ShimmerBluetooth.ShimmerIdentifier.MSG_IDENTIFIER_STATE_CHANGE:
-                    System.Diagnostics.Debug.Write(((Shimmer)sender).GetDeviceName() + " State = " + ((Shimmer)sender).GetStateString() + System.Environment.NewLine);
+                    System.Diagnostics.Debug.Write(((ShimmerBluetooth)sender).GetDeviceName() + " State = " + ((ShimmerBluetooth)sender).GetStateString() + System.Environment.NewLine);
                     int state = (int)eventArgs.getObject();
                     if (state == (int)ShimmerBluetooth.SHIMMER_STATE_CONNECTED)
                     {   
@@ -76,7 +76,7 @@ namespace ShimmerConsoleAppExample
                     }
                     else if (state == (int)ShimmerBluetooth.SHIMMER_STATE_NONE)
                     {
-
+                        System.Console.WriteLine("Shimmer is Disconnected");
                     }
                     else if (state == (int)ShimmerBluetooth.SHIMMER_STATE_STREAMING)
                     {
