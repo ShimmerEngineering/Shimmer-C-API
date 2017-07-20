df711330277223ad2318f441407d1c9715e9f04a
Jong Chern <jongchern@gmail.com>
2017-07-20 17:31:05 +0800

    limit 32 feet example to low noise accel

--- Shimmer32FeetConsoleAppExample/ShimmerConsoleAppExample/Program.cs
+++ Shimmer32FeetConsoleAppExample/ShimmerConsoleAppExample/Program.cs
@@ -21,7 +21,7 @@ namespace ShimmerConsoleAppExample
 
         public void start()
         {
-            int enabledSensors = ((int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_A_ACCEL | (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_GSR | (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_INT_A13);
+            int enabledSensors = ((int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_A_ACCEL);
 
             //shimmer = new Shimmer32Feet("ShimmerID1", "00:06:66:66:96:86");
             shimmer = new ShimmerLogAndStream32Feet("ShimmerID1", "00:06:66:66:96:A9", 102.4, 0, ShimmerBluetooth.GSR_RANGE_AUTO, enabledSensors, false, false, false, 1, 0, Shimmer3Configuration.EXG_EMG_CONFIGURATION_CHIP1, Shimmer3Configuration.EXG_EMG_CONFIGURATION_CHIP2, true);
