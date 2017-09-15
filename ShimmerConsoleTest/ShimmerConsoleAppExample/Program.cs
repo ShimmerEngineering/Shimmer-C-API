339eb1eeca54c249200f05495405f3d29b144219
Jong Chern <jongchern@gmail.com>
2017-09-15 14:03:02 +0800

    test for shimmer device, connect-stream-disconnect-repeat

--- ShimmerConsoleTest/ShimmerConsoleAppExample/Program.cs
+++ ShimmerConsoleTest/ShimmerConsoleAppExample/Program.cs
@@ -10,9 +10,10 @@ namespace ShimmerConsoleAppExample
 {
     class Program
     {
-        ShimmerLogAndStreamSystemSerialPort shimmer;
-        int count = -1;
+        ShimmerBluetooth shimmer;
+        int count = 0;
         int lastknowncount = -1;
+        Boolean stream = true;
         static void Main(string[] args)
         {
             /* Example of using 32 feet to scan for devices
@@ -30,29 +31,22 @@ namespace ShimmerConsoleAppExample
             
             //There are two main uses of the constructors, first one just connects to the device without setting and specific configurations
             //shimmer = new ShimmerSDBT("ShimmerID1", "COM15");
-            int enabledSensors = ((int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_A_ACCEL); // this is to enable Analog Accel also known as low noise accelerometer
+            int enabledSensors = ((int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_A_ACCEL | (int)ShimmerBluetooth.SensorBitmapShimmer3.SENSOR_D_ACCEL); // this is to enable Analog Accel also known as low noise accelerometer
             double samplingRate = 51.2;
             //byte[] defaultECGReg1 = new byte[10] { 0x00, 0xA0, 0x10, 0x40, 0x40, 0x2D, 0x00, 0x00, 0x02, 0x03 }; //see ShimmerBluetooth.SHIMMER3_DEFAULT_ECG_REG1
             //byte[] defaultECGReg2 = new byte[10] { 0x00, 0xA0, 0x10, 0x40, 0x47, 0x00, 0x00, 0x00, 0x02, 0x01 }; //see ShimmerBluetooth.SHIMMER3_DEFAULT_ECG_REG2
             byte[] defaultECGReg1 = ShimmerBluetooth.SHIMMER3_DEFAULT_TEST_REG1; //also see ShimmerBluetooth.SHIMMER3_DEFAULT_ECG_REG1
             byte[] defaultECGReg2 = ShimmerBluetooth.SHIMMER3_DEFAULT_TEST_REG2; //also see ShimmerBluetooth.SHIMMER3_DEFAULT_ECG_REG2
             //The constructor below allows the user to specify the shimmer configurations which is set upon connection to the device
-            shimmer = new ShimmerLogAndStreamSystemSerialPort("ShimmerID1", "COM12", 1, 0, 4, enabledSensors, false, false, false, 0, 0, defaultECGReg1, defaultECGReg2, false);
+            //shimmer = new ShimmerLogAndStreamSystemSerialPort("ShimmerID1", "COM12", 1, 0, 4, enabledSensors, false, false, false, 0, 0, defaultECGReg1, defaultECGReg2, false);
+            //shimmer = new ShimmerLogAndStreamSystemSerialPort("ShimmerID1", "COM35", 1, 0, 4, enabledSensors, false, false, false, 0, 0, defaultECGReg1, defaultECGReg2, false);
+
+            shimmer = new ShimmerLogAndStream32Feet("ShimmerID1", "00:06:66:66:96:A9", 1, 0, 4, enabledSensors, false, false, false, 0, 0, defaultECGReg1, defaultECGReg2, false);
+
             shimmer.UICallback += this.HandleEvent;
-            for (int i = 0; i < 10; i++)
-            {
-                count = i;
-                shimmer.Connect();
-                if (shimmer.GetState() == ShimmerBluetooth.SHIMMER_STATE_CONNECTED)
-                {
-                    shimmer.WriteSamplingRate(samplingRate);
-                    shimmer.WriteSensors(enabledSensors);
-                    shimmer.StartStreaming();
-                    Thread.Sleep(1000);
-                    shimmer.StopStreaming();
-                    shimmer.Disconnect();
-                }
-            }
+            shimmer.Connect();
+            
+            
         }
         public void HandleEvent(object sender, EventArgs args)
         {
@@ -66,8 +60,26 @@ namespace ShimmerConsoleAppExample
                     int state = (int)eventArgs.getObject();
                     if (state == (int)ShimmerBluetooth.SHIMMER_STATE_CONNECTED)
                     {
+
                         System.Diagnostics.Debug.Write("Connected");
                         System.Console.WriteLine(count + " " + "Connected");
+                        if (stream)
+                        {
+                            //Needs to be executed on a seperate thread, and a sleep to ensure everything on the current thread is completed
+                            new Thread(() =>
+                            {
+                                Thread.Sleep(500);
+                                shimmer.StartStreaming();
+                            }).Start();
+                        } else
+                        {
+                            new Thread(() =>
+                            {
+                                Thread.Sleep(500);
+                                shimmer.Disconnect();
+                            }).Start();
+                        }
+   
                     }
                     else if (state == (int)ShimmerBluetooth.SHIMMER_STATE_CONNECTING)
                     {
@@ -78,11 +90,19 @@ namespace ShimmerConsoleAppExample
                     {
                         System.Diagnostics.Debug.Write("Disconnected");
                         System.Console.WriteLine(count + " " + "Disconnected");
+                        stream = true;
+                        new Thread(() =>
+                        {
+                            Thread.Sleep(500);
+                            count++;
+                            shimmer.Connect();
+                        }).Start();
                     }
                     else if (state == (int)ShimmerBluetooth.SHIMMER_STATE_STREAMING)
                     {
                         System.Diagnostics.Debug.Write("Streaming");
                         System.Console.WriteLine(count + " " + "Streaming");
+                       
                     }
                     break;
                 case (int)ShimmerBluetooth.ShimmerIdentifier.MSG_IDENTIFIER_NOTIFICATION_MESSAGE:
@@ -94,6 +114,13 @@ namespace ShimmerConsoleAppExample
                     {
                         System.Console.WriteLine(count + " " + "AccelX: " + data.Data);
                         lastknowncount = count;
+                        stream = false;
+                        new Thread(() =>
+                        {
+                            Thread.Sleep(500);
+                            shimmer.StopStreaming();
+                        }).Start();
+
                     }
                     
                     break;
