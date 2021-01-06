46da8449ab303eaad5469b61c7cc4ee4b5a70525
Jong Chern <jongchern@gmail.com>
2021-01-06 12:25:41 +0800

    pair and scan apps

---
+++ 32FeetConsolePairApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using InTheHand.Net;
+using System.IO;
+
+
+
+namespace _32FeetConsoleAppLib
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length > 0)
+                {
+                    //"00066679E454"
+                    BluetoothAddress address = BluetoothAddress.Parse(args[0]);
+                    bool isPaired = BluetoothSecurity.PairRequest(address, "1234");
+                    if (isPaired)
+                    {
+                        BluetoothDeviceInfo bdinfo = new BluetoothDeviceInfo(address);
+                        bdinfo.SetServiceState(BluetoothService.SerialPort, true, true);
+                        Console.WriteLine(bdinfo.DeviceAddress + " Is Paired");
+                    }
+                }
+                
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Fail");
+            }
+
+        }
+    }
+}
--- 32FeetConsoleScanApp/Program.cs
+++ 32FeetConsoleScanApp/Program.cs
@@ -31,61 +31,12 @@ namespace _32FeetConsoleAppLib
                     if (device.DeviceName.Contains("Shimmer"))
                     {
                         shimmerDevices.Add(device);
+                        Console.WriteLine(device.DeviceAddress + ";" + device.DeviceName + ";" + device.Authenticated);
                     }
                 }
-                BluetoothDeviceInfo bdinfo = null;
-                if (shimmerDevices.Count > 1)
-                {
-                    System.Console.WriteLine("Multiple Shimmer Devices Detected, Please Key In Address You Want To Pair:");
-                    foreach (BluetoothDeviceInfo device in shimmerDevices)
-                    {
-                        System.Console.WriteLine(device.DeviceName + ";" + device.DeviceAddress);
-                    }
-                    string address = System.Console.ReadLine();
-                    foreach (BluetoothDeviceInfo device in shimmerDevices)
-                    {
-                        if (device.DeviceAddress.ToString().Equals(address))
-                        {
-                            bdinfo = device;
-                        }
-                    }
-                }else if (shimmerDevices.Count == 1)
-                {
-                    bdinfo = shimmerDevices[0];
-                } else
-                {
-                    System.Console.WriteLine("No Shimmer Devices Detected");
-                }
-                if (bdinfo != null)
-                {
-                    // replace DEVICE_PIN here, synchronous method, but fast
-                    if (!bdinfo.Authenticated)
-                    {
-                        isPaired = BluetoothSecurity.PairRequest(bdinfo.DeviceAddress, "1234");
-                        if (isPaired)
-                        {
-                            bdinfo.SetServiceState(BluetoothService.SerialPort, true, true);
-                        }
-                    }
-                    else
-                    {
-                        isPaired = true;
-                    }
-                    if (isPaired)
-                    {
-                        // now it is paired
-                        Console.WriteLine(bdinfo.DeviceAddress+ ";" + bdinfo.DeviceName + ";Is Paired"); ;
-                    }
-                    else
-                    {
-                        // pairing failed
-                        Console.WriteLine(bdinfo.DeviceAddress + ";" + bdinfo.DeviceName + ";Is Not Paired");
-                    }
-                }
-                
             } catch (Exception ex)
             {
-                Console.WriteLine("Fail");
+                //Console.WriteLine("Fail");
             }
 
         }
