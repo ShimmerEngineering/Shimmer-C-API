4bf62aaa393b78aae4310294a692c4aa2716560e
Mas Azalya <masazalya@shimmersensing.com>
2021-06-16 23:28:05 +0800

    new dummy API

---
+++ DummyAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DummyAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
