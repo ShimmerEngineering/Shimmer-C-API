a4ff7f62ec428fe0e122610704ff1067d6fbde78
Mas Azalya <masazalya@shimmersensing.com>
2021-06-17 12:24:07 +0800

    added class library project

---
+++ DummyLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DummyLibrary
+{
+    public class Class1
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
