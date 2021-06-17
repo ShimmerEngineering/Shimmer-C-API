d12ed71a80cdf9b53fbf5219f54a9c9a7b9e17af
Mas Azalya <masazalya@shimmersensing.com>
2021-06-17 14:45:55 +0800

    test dotnet framework

---
+++ DotNetFrameworkAPI/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetFrameworkAPI
+{
+    public class Class1
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
