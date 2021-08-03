14335fdc2f015bcdba66381b90361d3fb74077eb
jyong15 <jyong@shimmersensing.com>
2021-08-03 09:48:26 +0800

    VCBA-36 #time 10m adding some structure to PlotManager

--- ShimmerAPI/ShimmerAPI/PlotManager.cs
+++ ShimmerAPI/ShimmerAPI/PlotManager.cs
@@ -6,5 +6,38 @@ namespace ShimmerAPI
 {
     class PlotManager
     {
+        public static List<int[]> ListOfTraceColorsCurrentlyUsed = new List<int[]>();
+
+        public static List<int[]> ListOfTraceColorsDefault = new List<int[]>()
+        {
+            //TODO fill in colors here, see AbstractPlotManager.java
+        };
+
+        public PlotManager()
+        {
+
+        }
+
+        public void AddSignalGenerateRandomColor(string[] channelStringArray)
+        {
+            AddSignal(channelStringArray);
+            ListOfTraceColorsCurrentlyUsed.Add(GenerateRandomColor());
+        }
+
+        public void FilterDataAndPlot(ObjectCluster ojc)
+        {
+            //TODO fill in logic here, see BasicPlotManagerPC.java
+        }
+
+        private void AddSignal(string[] channelStringArray)
+        {
+            //TODO fill in logic here, see AbstractPlotManager.java
+        }
+
+        private int[] GenerateRandomColor()
+        {
+            //TODO fill in logic here, see AbstractPlotManager.java
+            return null;
+        }
     }
 }
