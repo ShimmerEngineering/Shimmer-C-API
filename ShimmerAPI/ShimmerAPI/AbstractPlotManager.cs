951430f19d20b043a5f413746277d3728032ea26
jyong15 <jyong@shimmersensing.com>
2021-08-06 16:02:22 +0800

    VCBA-36 #time 10m Update AbstractPlotManager

--- ShimmerAPI/ShimmerAPI/AbstractPlotManager.cs
+++ ShimmerAPI/ShimmerAPI/AbstractPlotManager.cs
@@ -5,7 +5,7 @@ using ShimmerAPI.Utilities;
 
 namespace ShimmerAPI
 {
-    abstract class AbstractPlotManager: UtilShimmer
+    public abstract class AbstractPlotManager
     {
         //Add the methods from https://github.com/ShimmerEngineering/Shimmer-Java-Android-API/blob/Dev/ShimmerDriver/src/main/java/com/shimmerresearch/guiUtilities/AbstractPlotManager.java
         //Into this class
@@ -15,14 +15,14 @@ namespace ShimmerAPI
         private static List<byte[]> listOfTraceColorsDefault = new List<byte[]>()
         {
             //TODO fill in colors here, see AbstractPlotManager.java
-            SHIMMER_DEFAULT_COLOURS.colourShimmerOrange,
-            SHIMMER_DEFAULT_COLOURS.colourBrown,
-            SHIMMER_DEFAULT_COLOURS.colourCyanAqua,
-            SHIMMER_DEFAULT_COLOURS.colourPurple,
-            SHIMMER_DEFAULT_COLOURS.colourMaroon,
-            SHIMMER_DEFAULT_COLOURS.colourGreen,
-            SHIMMER_DEFAULT_COLOURS.colourShimmerGrey,
-            SHIMMER_DEFAULT_COLOURS.colourShimmerBlue
+            UtilShimmer.SHIMMER_DEFAULT_COLOURS.colourShimmerOrange,
+            UtilShimmer.SHIMMER_DEFAULT_COLOURS.colourBrown,
+            UtilShimmer.SHIMMER_DEFAULT_COLOURS.colourCyanAqua,
+            UtilShimmer.SHIMMER_DEFAULT_COLOURS.colourPurple,
+            UtilShimmer.SHIMMER_DEFAULT_COLOURS.colourMaroon,
+            UtilShimmer.SHIMMER_DEFAULT_COLOURS.colourGreen,
+            UtilShimmer.SHIMMER_DEFAULT_COLOURS.colourShimmerGrey,
+            UtilShimmer.SHIMMER_DEFAULT_COLOURS.colourShimmerBlue
         };
     }
 }
