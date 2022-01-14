5557db4665dc905db375a0db8f75547ae1113055
Jong Chern <jongchern@gmail.com>
2022-01-14 13:11:18 +0800

    first commit

---
+++ ShimmerBLE/VerisenseBLEDemoApp/VerisenseBLEDemoApp/VerisenseBLEDemoApp/VerisenseBLEDemoApp.Android/LocalFolderService.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using shimmer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VerisenseBLEDemoApp.Droid;
+
+[assembly:Xamarin.Forms.Dependency(typeof(LocalFolderService))]
+namespace VerisenseBLEDemoApp.Droid
+{
+    public class LocalFolderService : ILocalFolderService
+    {
+        public string GetAppLocalFolder()
+        {
+            return Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDocuments).Path;
+        }
+    }
+}
---
+++ ShimmerBLE/VerisenseBLEDemoApp/VerisenseBLEDemoApp/VerisenseBLEDemoApp/VerisenseBLEDemoApp.Android/UIHelperAndroid.cs
@@ -0,0 +1,22 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using ShimmerBLEAPI.Services;
+using System;
+using VerisenseBLEDemoApp.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(UIHelperAndroid))]
+namespace VerisenseBLEDemoApp.Droid
+{
+    [Activity(Label = "@string/app_name", Theme = "@android:style/Theme.Translucent.NoTitleBar")]
+    public class UIHelperAndroid : INativeUIService
+    {
+        public async void Invoke(Action action)
+        {
+            var handler = new Handler(Looper.MainLooper);
+            handler.Post(action);
+        }
+    }
+}
