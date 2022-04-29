cf75ef92c67a18862e1feaac7ccc3ddeb9efc84a
weiwentan23 <weiwentan@shimmersensing.com>
2022-04-29 10:09:29 +0800

    Add ble client

---
+++ ShimmerBLE/BLE.Client/BLE.Client.UWP/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+using shimmer.Services;
+using System.Threading.Tasks;
+
+namespace BLE.Client.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();            
+        }
+    }
+}
---
+++ ShimmerBLE/BLE.Client/BLE.Client/Pages/DescriptorDetailPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BLE.Client.ViewModels;
+using MvvmCross.Forms.Views;
+using Xamarin.Forms;
+
+namespace BLE.Client.Pages
+{
+    public partial class DescriptorDetailPage : MvxContentPage<DescriptorDetailViewModel>
+    {
+        public DescriptorDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
