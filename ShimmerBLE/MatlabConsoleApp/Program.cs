ede4fbf36abeb9ef84e322164ef6e0af16249235
Tan Wei Wen <weiwentan@shimmersensing.com>
2022-05-10 09:39:37 +0800

    update matlab console app

---
+++ ShimmerBLE/MatlabConsoleApp/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using shimmer.Models;
+using shimmer.Helpers;
+using static shimmer.Models.ShimmerBLEEventData;
+
+namespace MatlabConsoleApp
+{
+    internal class Program
+    {
+        static VerisenseBLEDeviceMatlab device;
+
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            //args[0] - uuid
+            //args[1] - sync / write prod config
+            //args[2] - bin file path / op config bytes
+
+            var result = await ConnectDevice(args[0]);
+            if (result)
+            {
+                if (args[1] == "DATA_SYNC")
+                {
+                    await StartDataSync(args[2]);
+                }
+                else if (args[1] == "WRITE_OP_CONFIG")
+                {
+                    await WriteOpConfig(args[2]);
+                }
+            }
+            await DisconnectDevice();
+
+            //var result = await ConnectDevice("00000000-0000-0000-0000-d02b463da2bb");
+            //if (result)
+            //{
+            //    //await StartDataSync("C:\\Users\\WeiWentan\\Desktop");
+            //    await WriteOpConfig("5A-97-00-00-00-30-30-00-7F-00-D8-0F-09-16-24-0C-80-00-00-00-00-00-00-00-00-00-00-00-00-00-03-F4-18-3C-00-0A-0F-00-18-3C-00-0A-0F-00-18-3C-00-0A-0F-00-FF-FF-AA-01-03-3C-00-0E-00-00-63-28-CC-CC-1E-00-0A-00-00-00-00-01");
+            //}
+        }
+
+        static async System.Threading.Tasks.Task<bool> ConnectDevice(string uuid)
+        {
+            Console.WriteLine("CONNECT");
+            device = new VerisenseBLEDeviceMatlab(uuid, "");
+            device.ShimmerBLEEvent += ShimmerDevice_BLEEvent;
+            bool result = await device.Connect(true);
+            if (result)
+            {
+                Console.WriteLine("---------------------------------------------------------------");
+                Console.WriteLine("Successfully connected to the device! UUID: " + uuid);
+                Console.WriteLine("Device Version: " + device.GetProductionConfig().REV_HW_MAJOR + "." + device.GetProductionConfig().REV_HW_MINOR);
+                Console.WriteLine("Firmware Version: " + device.GetProductionConfig().REV_FW_MAJOR + "." + device.GetProductionConfig().REV_FW_MINOR + "." + device.GetProductionConfig().REV_FW_INTERNAL);
+                Console.WriteLine("\nBT state: " + device.GetVerisenseBLEState() + "\nUUID: " + device.Asm_uuid + "\nBattery: " + device.GetStatus().BatteryPercent + "%");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect device! UUID: " + uuid);
+            }
+            return false;
+        }
+
+        static async System.Threading.Tasks.Task<bool> StartDataSync(string path)
+        {
+            if (!System.IO.Directory.Exists(path))
+            {
+                Console.WriteLine("Please enter a valid path");
+                return false;
+            }
+            VerisenseBLEDeviceMatlab.path = path;
+
+            var data = await device.ExecuteRequest(RequestType.TransferLoggedData);
+            if (device != null)
+            {
+                device.ShimmerBLEEvent -= ShimmerDevice_BLEEvent;
+            }
+            device.ShimmerBLEEvent += ShimmerDevice_BLEEvent;
+            if(data != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static async System.Threading.Tasks.Task<bool> WriteOpConfig(string bytesstring)
+        {
+            byte[] opconfigbytes = BitHelper.MSBByteArray(bytesstring.Replace("-", "")).ToArray();
+            var result = await device.ExecuteRequest(RequestType.WriteOperationalConfig, opconfigbytes);
+            if (result != null)
+            {
+                Console.WriteLine("Write Success");
+                return true;
+            }
+            return false;
+        }
+
+        static void ShimmerDevice_BLEEvent(object sender, ShimmerBLEEventData e)
+        {
+            if (e.CurrentEvent == VerisenseBLEEvent.NewDataPacket)
+            {
+                
+            }
+        }
+
+        static async System.Threading.Tasks.Task<bool> DisconnectDevice()
+        {
+            var result = await device.Disconnect();
+            if (result)
+            {
+                device.ShimmerBLEEvent -= ShimmerDevice_BLEEvent;
+                Console.WriteLine("\nUUID: " + device.Asm_uuid + " attempt disconnect \nResult: " + result + "\nNew BLE Status: " + device.GetVerisenseBLEState());
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ShimmerBLE/MatlabConsoleApp/VerisenseBLEDeviceMatlab.cs
@@ -0,0 +1,75 @@
+﻿using shimmer.Communications;
+using ShimmerBLEAPI.Devices;
+using System;
+using System.IO;
+
+namespace MatlabConsoleApp
+{
+    internal class VerisenseBLEDeviceMatlab : VerisenseBLEDevice
+    {
+        public static string path;
+        public VerisenseBLEDeviceMatlab(string uuid, string id) : base(uuid, id)
+        {
+
+        }
+
+        //protected override void StartExecuteRequestTimer()
+        //{
+        //}
+
+        //protected override void StartDataRequestTimer()
+        //{
+        //}
+
+        protected override void InitializeRadio()
+        {
+            BLERadio = new RadioPlugin32Feet();
+        }
+
+        protected override void createBinFile(bool crcError)
+        {
+            try
+            {
+                //var asm = RealmService.GetSensorbyID(Asm_uuid.ToString());
+                //var trialSettings = RealmService.LoadTrialSettings();
+
+                //var participantID = asm.ParticipantID;
+                String sensorID = Asm_uuid.ToString();
+                try
+                {
+                    sensorID = GetSensorID();
+                }
+                catch (Exception ex) // if production config wasnt read default to the UUID
+                {
+                    sensorID = Asm_uuid.ToString();
+                    AdvanceLog(ex.Message, "Defaulting to UUID", dataFileName, ASMName);
+                }
+                binFileFolderDir = string.Format("{0}/{1}/{2}/BinaryFiles", GetTrialName(), GetParticipantID(), sensorID);
+                //string path = ApplicationData.Current.LocalFolder.Path;
+                var folder = Path.Combine(path, binFileFolderDir);
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+                if (crcError)
+                {
+                    dataFileName = string.Format("{0}_{1}_{2}.bin", DateTime.Now.ToString("yyMMdd_HHmmss"), PayloadIndex.ToString("00000"), BadCRC);
+                }
+                else
+                {
+                    dataFileName = string.Format("{0}_{1}.bin", DateTime.Now.ToString("yyMMdd_HHmmss"), PayloadIndex.ToString("00000"));
+                }
+
+                AdvanceLog(LogObject, "BinFileNameCreated", dataFileName, ASMName);
+                dataFilePath = Path.Combine(folder, dataFileName);
+
+                AdvanceLog(LogObject, "BinFileCreated", dataFilePath, ASMName);
+            }
+            catch (Exception ex)
+            {
+                AdvanceLog(LogObject, "BinFileCreatedException", ex, ASMName);
+            }
+        }
+    }
+}
