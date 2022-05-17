421f22f73a375512d4cd89d7cb0c13aae265ecb9
JongChern <jongchern@gmail.com>
2022-05-17 21:41:31 +0800

    add default pairing key generator

---
+++ ShimmerBLE/VerisenseBLEDemoApp/VerisenseBLEDemoApp/Advance/VerisenseBLEPairingKeyGenerator.cs
@@ -0,0 +1,16 @@
+﻿using ShimmerBLEAPI.Communications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VerisenseBLEDemoApp.Advance
+{
+    public class VerisenseBLEPairingKeyGenerator : IBLEPairingKeyGenerator
+    {
+        public string CalculatePairingPin(string deviceName)
+        {
+            string defaultPin = "123456";
+            return defaultPin;
+        }
+    }
+}
