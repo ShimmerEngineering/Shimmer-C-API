61380b81c344d2760f2c53ba606870b6275172e4
Tan Wei Wen <weiwentan@shimmersensing.com>
2022-07-29 13:41:20 +0800

    serial port for uwp working

---
+++ ShimmerBLE/ShimmerBLEAPI/Communications/IVerisenseSerialPortManager.cs
@@ -0,0 +1,13 @@
+﻿using ShimmerBLEAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace shimmer.Communications
+{
+    public interface IVerisenseSerialPortManager
+    {
+        List<VerisenseSerialDevice> GetListOfSerialDevices();
+        Task<bool> StartScanForSerialPorts();
+        void StopScanForSerialPorts();
+    }
+}
---
+++ ShimmerBLE/ShimmerBLEAPI/Models/VerisenseSerialDevice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShimmerBLEAPI.Models
+{
+    public class VerisenseSerialDevice
+    {
+        public string Id { get; set; }
+
+        public VerisenseSerialDevice(string id)
+        {
+            Id = id;
+        }
+    }
+}
