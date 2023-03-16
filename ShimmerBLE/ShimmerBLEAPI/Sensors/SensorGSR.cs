470d86479e91fc885c3cf7e26cd3b83d395fe94a
Tan Wei Wen <weiwentan@shimmersensing.com>
2023-03-16 10:32:31 +0800

    fixed possibility out-of-range GSR values to be parsed

--- ShimmerBLE/ShimmerBLEAPI/Sensors/SensorGSR.cs
+++ ShimmerBLE/ShimmerBLEAPI/Sensors/SensorGSR.cs
@@ -423,6 +423,11 @@ namespace shimmer.Sensors
             {
                 gsrResistancekOHMs = UtilCalibration.NudgeDouble(gsrResistancekOHMs, ((double[])GSRRangeSetting.GetSettingsValue())[0], ((double[])GSRRangeSetting.GetSettingsValue())[1]);
             }
+            else
+            {
+                /* If auto-range is enabled, limit the lower range of the resistance due to circuit design */
+                return Math.Max(((double[])GSRRangeSetting.GetSettingsValue())[0], gsrResistancekOHMs);
+            }
             return gsrResistancekOHMs;
         }
         /// <summary>
