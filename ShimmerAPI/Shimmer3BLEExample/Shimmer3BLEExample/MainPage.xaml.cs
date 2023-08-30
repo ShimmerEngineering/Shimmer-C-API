a5c26c099b85f2bad91b9ca73e18433fcecf27a7
JongChern <jongchern@gmail.com>
2023-08-30 17:34:56 +0800

    temp

---
+++ ShimmerAPI/Shimmer3BLEExample/Shimmer3BLEExample/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Shimmer3BLEExample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ShimmerAPI.ShimmerLogAndStreamBLE device = new ShimmerAPI.ShimmerLogAndStreamBLE("e8eb1b9767ad", "e8eb1b9767ad");
+            device.Connect();
+        }
+    }
+}
