2d5771cd558c8c14453676bd81b815dbf201d28f
JongChern <jongchern@gmail.com>
2025-02-03 14:03:24 +0800

    Update LiteProtocolInstructionSet.cs

--- ShimmerAPI/ShimmerAPI/LiteProtocolInstructionSet.cs
+++ ShimmerAPI/ShimmerAPI/LiteProtocolInstructionSet.cs
@@ -374,10 +374,7 @@ namespace com.shimmerresearch.radioprotocol {
         [pbr::OriginalName("GET_VBATT_COMMAND")] GetVbattCommand = 149,
         [pbr::OriginalName("GET_CALIB_DUMP_COMMAND")] GetCalibDumpCommand = 154,
         [pbr::OriginalName("GET_I2C_BATT_STATUS_COMMAND_VALUE")] GetI2CBattStatusCommandValue = 158,
-        [pbr::OriginalName("GET_BMP280_CALIBRATION_COEFFICIENTS_COMMAND")] GetBmp280CalibrationCoefficientsCommand = 159,
-        [pbr::OriginalName("GET_BMP390_PRESS_CALIBRATION_COEFFICIENTS_COMMAND")] GetBmp390CalibrationCoefficientsCommand = 166,
-        [pbr::OriginalName("BMP390_CALIBRATION_COEFFICIENTS_RESPONSE")] Bmp390CalibrationCoefficientsResponse = 165,
-           
+        [pbr::OriginalName("GET_BMP280_CALIBRATION_COEFFICIENTS_COMMAND")] GetBmp280CalibrationCoefficientsCommand = 159,  
       }
 
       public enum InstructionsResponse {
