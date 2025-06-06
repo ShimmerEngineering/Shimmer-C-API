56744d74d519b25201664d3b071c312ea85cdcf8
Danesh Mariapan <161300025+dmariapan-shimmer@users.noreply.github.com>
2025-06-06 14:32:53 +0800

    Updated setup layout and generation for signal checkboxes programatically

--- ShimmerCapture/ShimmerCapture/Control.cs
+++ ShimmerCapture/ShimmerCapture/Control.cs
@@ -222,53 +222,41 @@ namespace ShimmerAPI
                 //ZedGraphControl3.Size = new System.Drawing.Size(this.Size.Width - 1000, ZedGraphControl3.Size.Height);
             }
 
-            CheckBoxArrayGroup1 = new CheckBox[60] { checkBox1, checkBox2, checkBox3, checkBox4, checkBox5, checkBox6, 
-            checkBox7, checkBox8, checkBox9, checkBox10, checkBox11, checkBox12, checkBox13, checkBox14, 
-            checkBox15, checkBox16, checkBox17, checkBox18, checkBox19, checkBox20, checkBox21, 
-            checkBox22, checkBox23, checkBox24, checkBox25, checkBox26, checkBox27, checkBox28, 
-            checkBox29, checkBox30,
-            checkBox31, checkBox32, checkBox33, checkBox34, checkBox35, checkBox36,
-            checkBox37, checkBox38, checkBox39, checkBox40, checkBox41, checkBox42, checkBox43, checkBox44,
-            checkBox45, checkBox46, checkBox47, checkBox48, checkBox49, checkBox50, checkBox51,
-            checkBox52, checkBox53, checkBox54, checkBox55, checkBox56, checkBox57, checkBox58,
-            checkBox59, checkBox60 };
-            foreach (var checkBox in CheckBoxArrayGroup1)
-            {
-                checkBox.CheckedChanged += new EventHandler(CheckBoxArrayGroup1_CheckedChanged);
-            }
-
-            CheckBoxArrayGroup2 = new CheckBox[60] { checkBox61, checkBox62, checkBox63, checkBox64, checkBox65, checkBox66,
-            checkBox67, checkBox68, checkBox69, checkBox70, checkBox71, checkBox72, checkBox73, checkBox74,
-            checkBox75, checkBox76, checkBox77, checkBox78, checkBox79, checkBox80, checkBox81,
-            checkBox82, checkBox83, checkBox84, checkBox85, checkBox86, checkBox87, checkBox88,
-            checkBox89, checkBox90,
-            checkBox91, checkBox92, checkBox93, checkBox94, checkBox95, checkBox96,
-            checkBox97, checkBox98, checkBox99, checkBox100, checkBox101, checkBox102, checkBox103, checkBox104,
-            checkBox105, checkBox106, checkBox107, checkBox108, checkBox109, checkBox110, checkBox111,
-            checkBox112, checkBox113, checkBox114, checkBox115, checkBox116, checkBox117, checkBox118,
-            checkBox119, checkBox120 };
-            foreach (var checkBox in CheckBoxArrayGroup2)
-            {
-                checkBox.CheckedChanged += new EventHandler(CheckBoxArrayGroup2_CheckedChanged);
-            }
-
-            CheckBoxArrayGroup3 = new CheckBox[60] { checkBox121, checkBox122, checkBox123, checkBox124, checkBox125, checkBox126,
-            checkBox127, checkBox128, checkBox129, checkBox130, checkBox131, checkBox132, checkBox133, checkBox134,
-            checkBox135, checkBox136, checkBox137, checkBox138, checkBox139, checkBox140, checkBox141,
-            checkBox142, checkBox143, checkBox144, checkBox145, checkBox146, checkBox147, checkBox148,
-            checkBox149, checkBox150,
-            checkBox151, checkBox152, checkBox153, checkBox154, checkBox155, checkBox156,
-            checkBox157, checkBox158, checkBox159, checkBox160, checkBox161, checkBox162, checkBox163, checkBox164,
-            checkBox165, checkBox166, checkBox167, checkBox168, checkBox169, checkBox170, checkBox171,
-            checkBox172, checkBox173, checkBox174, checkBox175, checkBox176, checkBox177, checkBox178,
-            checkBox179, checkBox180 };
-            foreach (var checkBox in CheckBoxArrayGroup3)
-            {
-                checkBox.CheckedChanged += new EventHandler(CheckBoxArrayGroup3_CheckedChanged);
-            }
+            // Dynamically create and add checkboxes for each panel
+            CheckBoxArrayGroup1 = CreateCheckBoxesForPanel(panel1, 1, CheckBoxArrayGroup1_CheckedChanged);
+            CheckBoxArrayGroup2 = CreateCheckBoxesForPanel(panel2, 61, CheckBoxArrayGroup2_CheckedChanged);
+            CheckBoxArrayGroup3 = CreateCheckBoxesForPanel(panel3, 121, CheckBoxArrayGroup3_CheckedChanged);
 
             InitializeGraphs();
             initializeExGLeadOff();
+        }
+
+        private CheckBox[] CreateCheckBoxesForPanel(Panel panel, int startingIndex, EventHandler eventHandler)
+        {
+            const int numCheckBoxes = 60;   // 60 Checkboxes per Panel
+            const int rowHeight = 20;       // Height of row
+            const int colXLeft = 1;         // Left column X
+            const int colXRight = 231;      // Right column X
+            const int startY = 9;           // Initial Y position
+            CheckBox[] checkBoxes = new CheckBox[numCheckBoxes];
+
+            for (int i = 0; i < numCheckBoxes; i++)
+            {
+                CheckBox cb = new CheckBox();
+                cb.Visible = false;
+                cb.AutoSize = true;
+
+                // Calculate row & point coords
+                int row = i / 2;
+                int x = (i % 2 == 0) ? colXLeft : colXRight;
+                int y = startY + row * (rowHeight);
+
+                cb.Location = new Point(x, y);
+                cb.CheckedChanged += eventHandler;
+                checkBoxes[i] = cb;
+                panel.Controls.Add(cb);
+            }
+            return checkBoxes;
         }
 
         public void ChangeStatusLabel(string text)
