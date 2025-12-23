9742ae8b746e93a054b2617ff5943600d553e3e4
JongChern <jongchern@gmail.com>
2025-12-23 15:41:55 +0800

    update

---
+++ ShimmerSDLogExample/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ShimmerAPI;
+
+Console.WriteLine("Hello, World!");
+
+ShimmerSDLog sdLog = new ShimmerSDLog("C:\\Users\\JC\\Shimmer_Workspace\\Backup\\2025-07-04_15.41.07\\e68ecd1ded3c\\data\\DefaultTrial_1751614671\\Shimmer_ED3C-000\\000");
+Console.WriteLine(sdLog.GetShimmerVersion());
+Console.WriteLine(sdLog.GetShimmerAddress());
